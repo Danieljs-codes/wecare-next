4929b43fe087d29eab375ead94280b02114923e3
Olamide <onabiyiolamide13@gmail.com>
2024-09-13 04:36:25 +0100

    I think this is the fix

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,7 @@ import { createClient } from '@libsql/client/web';
 import * as schema from './schema';
 
 const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
+  url: process.env.TURSO_CONNECTION_URL!,
   authToken: process.env.TURSO_AUTH_TOKEN,
 });
 
